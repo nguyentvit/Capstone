fc858ab7f7bf24a8455235b0563fe1205f533cc7
quycv <quycv@vnext.vn>
2025-06-13 03:23:13 +0700

    few

--- src/BuildingBlocks/obj/Debug/net8.0/BuildingBlocks.AssemblyInfo.cs
+++ src/BuildingBlocks/obj/Debug/net8.0/BuildingBlocks.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuildingBlocks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e98f4e3bbd90bf27d565546414e6059bfd2a4b37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2708bc56b06229c1cd568e7898ee27306358e3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuildingBlocks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuildingBlocks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
